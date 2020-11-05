28fb945aad74a365b235051794338e9674cd32bc
chenyongxiang <984358499@qq.com>
2020-11-05 20:14:35 +0800

    全排列

--- base/src/request/_202011104/AllPermutation.java
+++ base/src/request/_202011104/AllPermutation.java
@@ -9,7 +9,49 @@ package request._202011104;
 public class AllPermutation {
 
 
-    public static void main(String[] args) {
+    public void allPermutation (int i){
+
+        if (i < 1){
+            return;
+        }
+        //flag数值 用来存储 哪几个数值还没有被放置道数组中
+        boolean[] flags = new boolean[i];
+        for (int i1 = 0; i1 < i; i1++) {
+            flags[i1] = true;
+        }
+        allPermutation(i,0,new int[i],flags);
+
+
+    }
+
+    private void allPermutation(int i, int index, int[] arr, boolean[] flags) {
+        int flagIndex = -1;
+        if (index < i){
+
+            for (int j = 0; j < flags.length; j++) {
+                if (flags[j]){
+                    arr[index] = j+1;
+                    flagIndex = j;
+                    flags[j] = false;
+                    if (index == i-1){
+                        System.out.print("找到了一组排列:");
+                        for (int i1 : arr) {
+                            System.out.print(i1);
+                        }
+                        System.out.println();
+                    }else {
+                        allPermutation(i,index+1,arr,flags);
+                    }
+
+                }
+
+                if (flagIndex != -1){
+                    flags[flagIndex] = true;
+                }
+            }
+
+        }
+
 
     }
 }
